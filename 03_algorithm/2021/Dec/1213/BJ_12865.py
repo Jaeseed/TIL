8c3f5de7c683b5480998d3f327729bfc46807aa7
Jaesung <wotjd4725@gmail.com>
2021-12-18 00:10:21 +0900

    Resolve unresolved problems

--- 03_algorithm/2021/Dec/1213/BJ_12865.py
+++ 03_algorithm/2021/Dec/1213/BJ_12865.py
@@ -1,51 +1,45 @@
-# import time
-def packing():
-    global max_value
-    idx = 0
-    tong = []
-    total_weight = 0
-    total_value = 0
-    while idx < N:
-        w = things[idx][0]
-        v = things[idx][1]
-        if total_weight + w <= K:
-            total_weight += w
-            total_value += v
-            tong.append([w,v])
-            idx += 1
-        else:
-            flag = 0
-            if max_value < total_value:
-                max_value = total_value
-            if len(tong) == 0:
-                return
-            before_weight = tong[-1][0]
-            for i in range(idx,N):
-                if things[i][0] > before_weight:
-                    idx = i
-                    flag = 1
-                    break
-            if flag == 0:
-                return
-            while total_weight + things[idx][0] > K:
-                if len(tong) == 0:
-                    return
-                w, v = tong.pop()
-                total_weight -= w
-                total_value -= v
-    if max_value < total_value:
-        max_value = total_value
-    return
+## 2차원 dp
+# N, K = map(int, input().split())
+# things = []
+# for n in range(N):
+#     weight, value = map(int,input().split())
+#     if value == 0:
+#         continue
+#     things.append([weight,value])
+# new_length = len(things)
+# dp = [[0] * (K+1) for _ in range(new_length+1)]
+# for i in range(1, new_length+1):
+#     w = things[i-1][0]
+#     v = things[i-1][1]
+#     for j in range(1, K+1):
+#         if j >= w:
+#             dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
+#         else:
+#             dp[i][j] = dp[i-1][j]
+# print(dp[new_length][K])
 
 
-N, K = map(int, input().split())
+## 1차원 dp
+# 변수 받기
+N, K = map(int,input().split())
+# 물건 목록 리스트
 things = []
-max_value = 0
 for n in range(N):
-    thing, weight = map(int, input().split())
-    things.append([thing, weight])
-# start = time.time()
-things.sort(key=lambda x: (x[0], -x[1]))
-packing()
-print(max_value)
-# print("time :", time.time() - start)
+    weight, value = map(int,input().split())
+    # 가치 0이면 필요 없으니 버리기
+    if value == 0:
+        continue
+    things.append([weight, value])
+# 물건 목록 리스트 길이 구하기
+new_length = len(things)
+# dp 리스트 선언
+dp = [0] * (K+1)
+for i in range(new_length):
+    # weight, value 받기
+    w = things[i][0]
+    v = things[i][1]
+    # 뒤에서부터 오면서 가치 비교 및 수정
+    for j in range(K, w-1, -1):
+        if j >= w:
+            dp[j] = max(dp[j], dp[j-w] + v)
+print(dp[K])
--- 03_algorithm/2021/Dec/1214/BJ_4991.py
+++ 03_algorithm/2021/Dec/1214/BJ_4991.py
@@ -7,57 +7,78 @@ dc = [0, 0, -1, 1]
 def bfs(sr, sc, er, ec):
     qu = deque()
     qu.append([sr, sc, 0])
-    visited = [[0] * w in range(h)]
+    this_visited = [[0] * w for _ in range(h)]
     while qu:
-        row, col, cnt = qu.popleft()
+        tr, tc, cnt = qu.popleft()
         for i in range(4):
-            nr = row + dr[i]
-            nc = col + dc[i]
-            if nr >= h or nr < 0 or nc >= w or nc < 0 or visited[nr][nc] == 1 or MAP[nr][nc] == 'x': continue
+            nr = tr + dr[i]
+            nc = tc + dc[i]
+            if nr >= h or nr < 0 or nc >= w or nc < 0 or this_visited[nr][nc] == 1 or MAP[nr][nc] == 'x': continue
             if nr == er and nc == ec:
                 return cnt + 1
+            this_visited[nr][nc] = 1
             qu.append([nr, nc, cnt + 1])
     return -1
-def calculate_min_distance():
-    min_distance = 2e29
 
 
+def calculate_min_distance(now, distance, cnt):
+    global min_distance
+    if distance > min_distance:
+        return
+    if cnt == dusts_cnt:
+        if min_distance > distance:
+            min_distance = distance
+        return
+    for i in range(1, dusts_cnt + 1):
+        if visited[i] == 1:
+            continue
+        visited[i] = 1
+        calculate_min_distance(i, distance + adj[now][i], cnt + 1)
+        visited[i] = 0
+    return
 
-w, h = map(int, input().split())
-MAP = [' '.join(input()).split() for _ in range(h)]
-dust_tong = []
-for r in range(h):
-    for c in range(w):
-        if MAP[r][c] == '*':
-            dust_tong.append([r, c])
-        if MAP[r][c] == 'o':
-            MAP[r][c] = '.'
-            cleaner = [r, c]
-dusts_cnt = len(dust_tong)
-ans = 0
-adj = [[0] * dusts_cnt for _ in range(dusts_cnt)]
-for d in range(dusts_cnt):
-    row, col = dust_tong[d][0], dust_tong[d][0]
-    start_r, start_c = cleaner[0], cleaner[1]
-    result = bfs(start_r, start_c, row, col)
-    if result == -1:
-        ans = -1
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
         break
+    MAP = [' '.join(input()).split() for _ in range(h)]
+    dust_tong = []
+    for r in range(h):
+        for c in range(w):
+            if MAP[r][c] == '*':
+                dust_tong.append([r, c])
+            if MAP[r][c] == 'o':
+                MAP[r][c] = '.'
+                cleaner = [r, c]
+    dusts_cnt = len(dust_tong)
+    ans = 0
+    adj = [[0] * (dusts_cnt + 1) for _ in range(dusts_cnt + 1)]
+    for d in range(dusts_cnt):
+        row, col = dust_tong[d][0], dust_tong[d][1]
+        start_r, start_c = cleaner[0], cleaner[1]
+        result = bfs(start_r, start_c, row, col)
+        if result == -1:
+            ans = -1
+            break
+        else:
+            adj[0][d + 1], adj[d + 1][0] = result, result
+    if ans == 0:
+        for start in range(dusts_cnt):
+            start_r, start_c = dust_tong[start][0], dust_tong[start][1]
+            for end in range(dusts_cnt):
+                if start == end: continue
+                end_r, end_c = dust_tong[end][0], dust_tong[end][1]
+                result = bfs(start_r, start_c, end_r, end_c)
+                adj[start + 1][end + 1], adj[end + 1][start + 1] = result, result
+        min_distance = 2e29
+        for s in range(1, dusts_cnt + 1):
+            visited = [0] * (dusts_cnt + 1)
+            calculate_min_distance(s, adj[0][s], 0)
+        print(min_distance)
+
     else:
-        adj[0][d + 1], adj[d + 1][0] = result, result
-if ans == 0:
-    for start in range(dusts_cnt):
-        start_r, start_c = dust_tong[start][0], dust_tong[start][1]
-        for end in range(dusts_cnt):
-            if start == end: continue
-            end_r, end_c = dust_tong[end][0], dust_tong[end][1]
-            result = bfs(start_r, start_c, end_r, end_c)
-            adj[start + 1][end + 1], adj[end + 1][start + 1] = result, result
-    calculate_min_distance()
-
-else:
-    print(-1)
-
+        print(-1)
 # from collections import deque
 #
 # dr = [-1, 1, 0, 0]
