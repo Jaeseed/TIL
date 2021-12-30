fd228515bb0db3199285d7733f82715ea4d0389d
Jaesung <wotjd4725@gmail.com>
2021-12-31 02:11:18 +0900

    algo TIL

--- 03_algorithm/2021/Dec/1230/BJ_2933.py
+++ 03_algorithm/2021/Dec/1230/BJ_2933.py
@@ -17,9 +17,9 @@ def bfs(r, c):
         for j in range(4):
             nr = r + dr[j]
             nc = c + dc[j]
-            if j == 1 and nr == R:
+            if nr < 0 or nc >= C or nc < 0 or MAP[nr][nc] == '.' or visited[nr][nc] == 1 or used[nr][nc] == 1: continue
+            if nr == R-1:
                 return True
-            if nr < 0 or nc >= C or nc < 0 or MAP[nr][nc] == '.' or visited[nr][nc] == 1: continue
             visited[nr][nc] = 1
             qu.append([nr, nc])
     return False
@@ -29,26 +29,29 @@ def find_min_cnt():
     min_cnt = 100
     for r,c in bottom_list:
         cnt = 0
-        now_r = r
-        while True:
-            cnt += 1
-            now_r += 1
-            if now_r >= R or MAP[now_r][c] == 'x':
+        now_r = r+1
+        flag = 0
+        while now_r < R:
+            if visited[now_r][c] == 1:
+                cnt = min_cnt
                 break
+            if MAP[now_r][c] == 'x': break
+            now_r += 1
+            cnt += 1
         min_cnt = min(min_cnt, cnt)
-    return min_cnt - 1
+    return min_cnt
 
 
 def gravity(jump):
+    global used
+    visited_col = [0] * C
     for r, c in bottom_list:
-        now_r = r + jump
-        while now_r-jump > 0:
-            visited[now_r][c] = 1
-            if MAP[now_r-jump][c] == 'x':
-                MAP[now_r - jump][c], MAP[now_r][c] = MAP[now_r][c], MAP[now_r - jump][c]
-                now_r -= 1
-            else:
-                jump += 1
+        if visited_col[c] == 1: continue
+        visited_col[c] = 1
+        for now_r in range(R-1,-1,-1):
+            if MAP[now_r][c] == 'x' and visited[now_r][c] == 1:
+                MAP[now_r + jump][c], MAP[now_r][c] = MAP[now_r][c], MAP[now_r + jump][c]
+                used[now_r][c] = 1
     return
 
 
@@ -68,19 +71,24 @@ for n in range(N):
             if MAP[row][col] == 'x':
                 MAP[row][col] = '.'
                 break
-    flag = 0
-    visited = [[0] * C for _ in range(R)]
+    cluster = []
     for i in range(4):
-        if i == 1: continue
         new_row = row + dr[i]
         new_col = col + dc[i]
-        if MAP[new_row][new_col] == 'x' and visited[new_row][new_col] == 0:
-            bottom_list = []
+        if 0 <= new_row < R and 0 <= new_col < C and MAP[new_row][new_col] == 'x':
             if new_row == R - 1: continue
-            if bfs(new_row, new_col):
+            cluster.append([new_row,new_col])
+    visited = [[0] * C for _ in range(R)]
+    used = [[0] * C for _ in range(R)]
+    for cr, cc in cluster:
+        if visited[cr][cc] == 0:
+            visited = [[0] * C for _ in range(R)]
+            bottom_list = []
+            if bfs(cr, cc):
                 continue
             else:
                 des_cnt = find_min_cnt()
-                gravity(des_cnt)
+                if des_cnt > 0:
+                    gravity(des_cnt)
 for row in range(R):
     print(''.join(MAP[row]))
