28687ad3a49bd7d56272fc4a791c8e7bc381eb2b
Jaeseed <wotjd4725@gmail.com>
2022-02-12 16:13:56 +0900

    algo TIL

--- 03_algorithm/2022/02/0211/BJ_20061.py
+++ 03_algorithm/2022/02/0211/BJ_20061.py
@@ -3,7 +3,6 @@ import sys
 
 def move_block():
     global spot_g, spot_b, green, blue
-    row_g, col_b = 6, 6
     flag_g = 1
     flag_b = 1
     # green move down
@@ -19,7 +18,6 @@ def move_block():
         r = spot_g[i][0]
         c = spot_g[i][1]
         green[r + plus - 1][c] = 1
-        row_g = min(row_g, r + plus - 1)
 
     # blue move down
     plus = 0
@@ -33,16 +31,15 @@ def move_block():
     for i in range(len(spot_b)):
         r = spot_b[i][0]
         c = spot_b[i][1]
-        blue[r][c+ plus-1] = 1
-        col_b = min(col_b, c + plus - 1)
+        blue[r][c + plus - 1] = 1
 
-    return row_g, col_b
+    return
 
 
 def ans_plus():
-    global scores
-    deleted = [0,0,0,0]
-    for r in range(6):
+    global scores, green, blue
+    deleted = [0, 0, 0, 0]
+    for r in range(2, 6):
         cnt = 0
         for c in range(4):
             if green[r][c]:
@@ -51,62 +48,56 @@ def ans_plus():
             deleted[0] += 1
             deleted[1] = r
             scores += 1
-            for c in range(4):
-                green[r][c] = 0
 
-    for c in range(6):
+    for c in range(2, 6):
         cnt = 0
         for r in range(4):
             if blue[r][c]:
                 cnt += 1
-        if cnt == 6:
+        if cnt == 4:
             deleted[2] += 1
             deleted[3] = c
             scores += 1
-            for r in range(4):
-                blue[r][c] = 0
-
     return deleted
 
 
 def gravity(deleted):
     global green, blue
-    total_cnt_g, end_g, total_cnt_b, end_b = deleted
-    distance_g = total_cnt_g
-    distance_b = total_cnt_b
-    while total_cnt_g:
-        for c in range(4):
-            green[end_g][c] = green[end_g - distance_g][c]
-            green[end_g][c] = 0
-        total_cnt_g -= 1
-    while total_cnt_b:
-        for r in range(4):
-            blue[r][end_b] = blue[r][end_b - distance_b]
-            blue[r][end_b] = 0
-        total_cnt_b -= 1
+    distance_g, end_g, distance_b, end_b = deleted
+    if end_g != 0:
+        for r in range(end_g, distance_g-1, -1):
+            for c in range(4):
+                green[r][c] = green[r - distance_g][c]
+                green[r - distance_g][c] = 0
+    if end_b != 0:
+        for c in range(end_b, distance_b-1, -1):
+            for r in range(4):
+                blue[r][c] = blue[r][c - distance_b]
+                blue[r][c - distance_b] = 0
     return
 
 
-def slicing(g, b):
+def slicing():
     global green, blue
-    slice_g = 2 - g
-    slice_b = 2 - b
-    if g < 2:
-        for c in range(4):
-            for i in range(1, slice_g+1):
-                green[-i][c] = 0
-        for c in range(4):
-            for r in range(5, 3 - slice_g, -1):
-                green[r][c] = green[r - 1][c]
-                green[r][c - 1] = 0
-    if b < 2:
-        for r in range(4):
-            for i in range(1, slice_b+1):
-                blue[r][-i] = 0
-        for r in range(4):
-            for c in range(5, 3 - slice_b, -1):
-                blue[r][c] = blue[r][c-1]
-                blue[r][c-1] = 0
+    tmp_g, tmp_b = 10, 10
+    for i in range(4):
+        for j in range(2):
+            if green[j][i]:
+                tmp_g = min(tmp_g, j)
+            if blue[i][j]:
+                tmp_b = min(tmp_b, j)
+    slice_g = 2 - tmp_g
+    slice_b = 2 - tmp_b
+    if tmp_g < 2:
+        for r in range(5, 1, -1):
+            for c in range(4):
+                green[r][c] = green[r - slice_g][c]
+                green[r - slice_g][c] = 0
+    if tmp_b < 2:
+        for c in range(5, 1, -1):
+            for r in range(4):
+                blue[r][c] = blue[r][c - slice_b]
+                blue[r][c - slice_b] = 0
     return
 
 
@@ -117,20 +108,19 @@ scores = 0
 for n in range(N):
     t, r_, c_ = map(int, sys.stdin.readline().split())
     if t == 1:
-        spot_g = [[0,c_]]
-        spot_b = [[r_,0]]
-    elif t == 3:
-        spot_g = [[0,c_],[1,c_]]
-        spot_b = [[r_,0],[r_+1,0]]
-    else:
+        spot_g = [[0, c_]]
+        spot_b = [[r_, 0]]
+    elif t == 2:
         spot_g = [[0, c_], [0, c_ + 1]]
-        spot_b = [[r_,0],[r_,1]]
-    value_g, value_b = move_block()
+        spot_b = [[r_, 0], [r_, 1]]
+    else:
+        spot_g = [[0, c_], [1, c_]]
+        spot_b = [[r_, 0], [r_ + 1, 0]]
+    move_block()
     ret = ans_plus()
     if ret[0] or ret[2]:
         gravity(ret)
-    if value_g <= 1 or value_b <= 1:
-        slicing(value_g,value_b)
+    slicing()
 total_cnt = 0
 for s in range(6):
     for d in range(4):
